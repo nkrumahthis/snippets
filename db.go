5708ed1e468561b0593862bb786fe7d6f0affa21
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-02-11 00:21:25 +0000

    fix table foreign keys

--- db.go
+++ db.go
@@ -1,6 +1,9 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type Database struct {
 	db *sql.DB
@@ -21,26 +24,32 @@ func (db *Database) Init() error{
 
 		CREATE TABLE IF NOT EXISTS snippets(
 			id VARCHAR(30) PRIMARY KEY,
-			user_id VARCHAR(30) NOT NULL FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE,
+			user_id VARCHAR(30) NOT NULL,
 			title TEXT,
 			code TEXT,
 			upvotes INTEGER DEFAULT 0,
 			downvotes INTEGER DEFAULT 0,
 			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-			updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+			updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 		);
 
 		CREATE TABLE IF NOT EXISTS upvotes (
 			id VARCHAR(30) PRIMARY KEY,
-			user_id VARCHAR(30) NOT NULL FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE,
-			timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+			user_id VARCHAR(30) NOT NULL,
+			timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
 		);
 		
 		CREATE TABLE IF NOT EXISTS downvotes(
 			id VARCHAR(30) PRIMARY KEY,
-			user_id VARCHAR(30) NOT NULL FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE,
-			timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+			user_id VARCHAR(30) NOT NULL,
+			timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE
 		);
 	`)
+	if err != nil {
+		fmt.Println("database initialized")
+	}
 	return err
 }
