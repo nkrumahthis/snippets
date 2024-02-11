27c3679f53a7909be9a4a8882a81b146d1036d51
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-02-11 00:21:09 +0000

    move database.db

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+func init() {
+
+}
+
 func main() {
-	fileServer := http.FileServer(http.Dir("./ui/dist"))
+	db, err := sql.Open("sqlite3", "temp/data.db")
+	if err != nil {
+		log.Fatal("Error opening database:", err)
+	}
+	defer db.Close()
+
+	database := Database{db}
+	if err := database.Init(); err != nil {
+		log.Fatal("Error initializing database:", err)
+	}
+
+	NewUserRepository(db)
+	
+	fileServer := http.FileServer(http.Dir("./_ui/dist"))
 	mux := http.NewServeMux()
 	mux.Handle("/", fileServer)
 
+	print("Starting server on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Println("server failed:", err)
 	}
